930f5bc71c74db97571bfb429ae547fa297889c3
Fred <Fred@CreativeProjects.Tech>
2019-06-19 16:35:23 +0100

    Mock system as Linux for these tests

--- tests/test_nice.py
+++ tests/test_nice.py
@@ -5,27 +5,27 @@ class TestNice(unittest.TestCase):
 
     def test_can_get_command_with_default_value(self):
         nice = Nice()
-        command = nice.get_command()
+        command = nice.get_command('Linux')
         self.assertEqual(command, "nice -n 10")
 
     def test_can_get_command_with_positive_value(self):
         nice = Nice(5)
-        command = nice.get_command()
+        command = nice.get_command('Linux')
         self.assertEqual(command, "nice -n 5")
 
     def test_can_get_command_with_negative_value(self):
         nice = Nice(-5)
-        command = nice.get_command()
+        command = nice.get_command('Linux')
         self.assertEqual(command, "nice -n -5")
 
     def test_can_get_command_with_value_too_low(self):
         nice = Nice(-100)
-        command = nice.get_command()
+        command = nice.get_command('Linux')
         self.assertEqual(command, "nice -n -20")
 
     def test_can_get_command_with_value_too_high(self):
         nice = Nice(100)
-        command = nice.get_command()
+        command = nice.get_command('Linux')
         self.assertEqual(command, "nice -n 20")
 
     def test_can_get_command_will_not_throw_exception_and_return_empty_string(self):
