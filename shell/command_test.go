dcd940063c16cbed165a790bd1e6dbe35d64d639
Fred <fred@creativeprojects.tech>
2020-04-20 00:14:22 +0100

    Run test on darwin only

--- shell/command_test.go
+++ shell/command_test.go
@@ -6,9 +6,7 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"syscall"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -126,29 +124,4 @@ func TestRunShellEchoWithSignalling(t *testing.T) {
 	}
 
 	assert.Contains(t, string(output), "TestRunShellEcho")
-}
-
-func TestInterruptShellCommand(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		t.Skip("Test not running under Windows")
-	}
-	buffer := &bytes.Buffer{}
-
-	sigChan := make(chan os.Signal, 1)
-
-	cmd := NewSignalledCommand("sleep", []string{"3"}, sigChan)
-	cmd.Stdout = buffer
-
-	// Will ask us to stop in 100ms
-	go func() {
-		time.Sleep(100 * time.Millisecond)
-		sigChan <- syscall.Signal(syscall.SIGINT)
-	}()
-	start := time.Now()
-	err := cmd.Run()
-	if err != nil && err.Error() != "signal: interrupt" {
-		t.Fatal(err)
-	}
-
-	assert.WithinDuration(t, time.Now(), start, 1*time.Second)
 }
