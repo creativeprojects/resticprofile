4aa7bcbaf81fffbeb5dd4554f29974d91fa26ee1
Fred <fred@creativeprojects.tech>
2020-06-27 13:50:23 +0100

    add OS & ARCH to panic data

--- main.go
+++ main.go
@@ -274,13 +274,15 @@ func randomBool() bool {
 func showPanicData() {
 	if r := recover(); r != nil {
 		message := `
-==========================================================
+===============================================================
 uh-oh! resticprofile crashed miserably :-(
-Please can you open an issue on github with these details:
-==========================================================
+Please can you open an issue on github including these details:
+===============================================================
 `
 		fmt.Fprintf(os.Stderr, message)
 		w := tabwriter.NewWriter(os.Stderr, 0, 0, 3, ' ', 0)
+		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "os", runtime.GOOS)
+		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "arch", runtime.GOARCH)
 		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "version", version)
 		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "commit", commit)
 		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "compiled", date)
@@ -288,6 +290,6 @@ Please can you open an issue on github with these details:
 		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "error", r)
 		_, _ = fmt.Fprintf(w, "\t%s:\t%s\n", "stack", string(debug.Stack()))
 		w.Flush()
-		fmt.Fprint(os.Stderr, "==========================================================\n")
+		fmt.Fprint(os.Stderr, "===============================================================\n")
 	}
 }
