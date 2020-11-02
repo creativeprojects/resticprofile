f3aa7b91d417f810eb77fdc15439b437b5ac5557
Fred <fred@creativeprojects.tech>
2020-11-02 00:28:13 +0000

    add tests on set and get PID from lock file

--- lock/lock.go
+++ lock/lock.go
@@ -17,12 +17,14 @@ type SetPID func(pid int)
 type Lock struct {
 	Lockfile string
 	file     *os.File
+	locked   bool
 }
 
 // NewLock creates a new lock
 func NewLock(filename string) *Lock {
 	return &Lock{
 		Lockfile: filename,
+		locked:   false,
 	}
 }
 
@@ -37,6 +39,7 @@ func (l *Lock) Release() {
 		_ = l.file.Close()
 	}
 	l.unlock()
+	l.locked = false
 }
 
 // Who owns the lock?
@@ -53,8 +56,16 @@ func (l *Lock) Who() (string, error) {
 // SetPID writes down the PID in the lock file.
 // You can run the method as many times as you want when the PID changes
 func (l *Lock) SetPID(pid int) {
+	if !l.locked {
+		return
+	}
 	// just add the PID on a newline
 	_, _ = l.file.WriteString(fmt.Sprintf("\n%d", pid))
+}
+
+// HasLocked check this instance (and only this one) has locked the file
+func (l *Lock) HasLocked() bool {
+	return l.locked
 }
 
 // LastPID returns the last PID written into the lock file.
@@ -82,6 +93,8 @@ func (l *Lock) lock() bool {
 		return false
 	}
 	// Leave the lock file open
+
+	l.locked = true
 
 	username := "unknown user"
 	currentUser, err := user.Current()
--- lock/lock_test.go
+++ lock/lock_test.go
@@ -27,13 +27,64 @@ func TestLockIsNotAvailable(t *testing.T) {
 	defer lock.Release()
 
 	assert.True(t, lock.TryAcquire())
+	assert.True(t, lock.HasLocked())
 
 	other := NewLock(tempfile)
 	defer other.Release()
 	assert.False(t, other.TryAcquire())
+	assert.False(t, other.HasLocked())
 
 	who, err := other.Who()
 	assert.NoError(t, err)
 	assert.NotEmpty(t, who)
 	assert.Regexp(t, regexp.MustCompile(`^[\-\\\w]+ on \w+, \d+-\w+-\d+ \d+:\d+:\d+ \w* from [\.\-\w]+$`), who)
+}
+
+func TestNoPID(t *testing.T) {
+	tempfile := filepath.Join(os.TempDir(), fmt.Sprintf("%s%d%d.tmp", "TestNoPID", time.Now().UnixNano(), os.Getpid()))
+	t.Log("Using temporary file", tempfile)
+	lock := NewLock(tempfile)
+	defer lock.Release()
+	lock.TryAcquire()
+
+	other := NewLock(tempfile)
+	defer other.Release()
+
+	pid, err := other.LastPID()
+	assert.Error(t, err)
+	assert.Empty(t, pid)
+}
+
+func TestSetOnePID(t *testing.T) {
+	tempfile := filepath.Join(os.TempDir(), fmt.Sprintf("%s%d%d.tmp", "TestSetPID", time.Now().UnixNano(), os.Getpid()))
+	t.Log("Using temporary file", tempfile)
+	lock := NewLock(tempfile)
+	defer lock.Release()
+	lock.TryAcquire()
+	lock.SetPID(11)
+
+	other := NewLock(tempfile)
+	defer other.Release()
+
+	pid, err := other.LastPID()
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf("%d", 11), pid)
+}
+
+func TestSetMorePID(t *testing.T) {
+	tempfile := filepath.Join(os.TempDir(), fmt.Sprintf("%s%d%d.tmp", "TestSetPID", time.Now().UnixNano(), os.Getpid()))
+	t.Log("Using temporary file", tempfile)
+	lock := NewLock(tempfile)
+	defer lock.Release()
+	lock.TryAcquire()
+	lock.SetPID(11)
+	lock.SetPID(12)
+	lock.SetPID(13)
+
+	other := NewLock(tempfile)
+	defer other.Release()
+
+	pid, err := other.LastPID()
+	assert.NoError(t, err)
+	assert.Equal(t, fmt.Sprintf("%d", 13), pid)
 }
