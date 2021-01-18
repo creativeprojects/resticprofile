9d931a3a052354cfc32b7d0c365253027582484f
Fred <Fred@CreativeProjects.Tech>
2021-01-18 15:42:07 +0000

    add test on Init

--- schedule/schedule_test.go
+++ schedule/schedule_test.go
@@ -3,8 +3,10 @@ package schedule
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
+	"github.com/creativeprojects/resticprofile/constants"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -27,4 +29,21 @@ func TestExecutableIsAbsoluteOnAllPlatforms(t *testing.T) {
 	require.NoError(t, err)
 	assert.True(t, filepath.IsAbs(binary))
 	t.Log(binary)
+}
+
+func TestInit(t *testing.T) {
+	err := Init()
+	defer Close()
+	require.NoError(t, err)
+}
+
+func TestCrondInit(t *testing.T) {
+	if runtime.GOOS == "windows" || runtime.GOOS == "darwin" {
+		t.Skip("crond not supported on this platform")
+	}
+	// TODO: the Scheduler package variable can make things tricky to test
+	Scheduler = constants.SchedulerCrond
+	err := Init()
+	require.NoError(t, err)
+	Scheduler = ""
 }
