b7edeb5bee4eaecaca971696871bb20dd127f3bd
jkellerer <jkellerer@users.noreply.github.com>
2021-04-25 15:40:08 +0200

    Updated contrib script systemd/send-error.sh (#49)
    Applied latest changes from ERROR env and added the profile to the mail

--- contrib/systemd/resticprofile-send-error.sh
+++ contrib/systemd/resticprofile-send-error.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 [[ -z "${PROFILE_NAME}" ]] || sendmail -t <<ERRMAIL
 To: $1
-From: "Resticprofile $(hostname -f)" <$USER@$(hostname -f)>
-Subject: Restic Failed: ${PROFILE_COMMAND} "${PROFILE_NAME}"
+From: "resticprofile $(hostname -f)" <$USER@$(hostname -f)>
+Subject: restic failed: ${PROFILE_COMMAND} "${PROFILE_NAME}"
 Content-Transfer-Encoding: 8bit
 Content-Type: text/plain; charset=UTF-8
 
 ${ERROR}
 
 ---- 
+COMMANDLINE:
+
+${ERROR_COMMANDLINE}
+
+----
+STDERR:
+
+${ERROR_STDERR}
+
+----
 DETAILS:
 
 $(systemctl status --full "resticprofile-${PROFILE_COMMAND}@profile-${PROFILE_NAME}")
+
+----
+CONFIG:
+
+$(resticprofile --name "${PROFILE_NAME}" show)
+
 ERRMAIL
 exit 0
