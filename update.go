c1570b36c83ab5d6c869586194d9a751d2b6bb5d
Fred <fred@creativeprojects.tech>
2024-08-17 22:37:25 +0100

    move update code back into main

--- update.go
+++ update.go
@@ -3,10 +3,17 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"io"
+	"os"
+	"runtime"
 
+	"github.com/creativeprojects/clog"
+	"github.com/creativeprojects/go-selfupdate"
 	"github.com/creativeprojects/resticprofile/config"
-	"github.com/creativeprojects/resticprofile/update"
+	"github.com/creativeprojects/resticprofile/term"
 )
 
 func init() {
@@ -30,9 +37,48 @@ func selfUpdate(_ io.Writer, ctx commandContext) error {
 	if !quiet && len(ctx.request.arguments) > 0 && (ctx.request.arguments[0] == "-q" || ctx.request.arguments[0] == "--quiet") {
 		quiet = true
 	}
-	err := update.ConfirmAndSelfUpdate(quiet, ctx.flags.verbose, version, true)
+	err := confirmAndSelfUpdate(quiet, ctx.flags.verbose, version, true)
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func confirmAndSelfUpdate(quiet, debug bool, version string, prerelease bool) error {
+	if debug {
+		selfupdate.SetLogger(clog.NewStandardLogger(clog.LevelDebug, clog.GetDefaultLogger()))
+	}
+	updater, _ := selfupdate.NewUpdater(
+		selfupdate.Config{
+			Validator:  &selfupdate.ChecksumValidator{UniqueFilename: "checksums.txt"},
+			Prerelease: prerelease,
+		})
+	latest, found, err := updater.DetectLatest(context.Background(), selfupdate.NewRepositorySlug("creativeprojects", "resticprofile"))
+	if err != nil {
+		return fmt.Errorf("unable to detect latest version: %w", err)
+	}
+	if !found {
+		return fmt.Errorf("latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+	}
+
+	if latest.LessOrEqual(version) {
+		clog.Infof("Current version (%s) is the latest", version)
+		return nil
+	}
+
+	// don't ask in quiet mode
+	if !quiet && !term.AskYesNo(os.Stdin, fmt.Sprintf("Do you want to update to version %s", latest.Version()), true) {
+		term.Println("Never mind")
+		return nil
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		return errors.New("could not locate executable path")
+	}
+	if err := updater.UpdateTo(context.Background(), latest, exe); err != nil {
+		return fmt.Errorf("unable to update binary: %w", err)
+	}
+	clog.Infof("Successfully updated to version %s", latest.Version())
 	return nil
 }
--- update_test.go
+++ update_test.go
@@ -1,13 +1,12 @@
 //go:build !no_self_update
 
-package update_test
+package main
 
 import (
 	"testing"
 
 	"github.com/creativeprojects/clog"
 	"github.com/creativeprojects/go-selfupdate"
-	"github.com/creativeprojects/resticprofile/update"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -20,7 +19,7 @@ func TestUpdate(t *testing.T) {
 	clog.SetTestLog(t)
 	defer clog.CloseTestLog()
 
-	err := update.ConfirmAndSelfUpdate(true, true, "0.0.1", false)
+	err := confirmAndSelfUpdate(true, true, "0.0.1", false)
 	require.ErrorIsf(t, err, selfupdate.ErrExecutableNotFoundInArchive, "error returned isn't wrapping %q but is instead: %q", selfupdate.ErrExecutableNotFoundInArchive, err)
 	assert.Contains(t, err.Error(), "resticprofile.test")
 }
