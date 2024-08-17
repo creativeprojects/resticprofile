398567703e5a350041b36c35bd37876863273cc3
Fred <fred@creativeprojects.tech>
2024-08-17 22:25:26 +0100

    this was needed :)

--- config/profile.go
+++ config/profile.go
@@ -127,6 +127,11 @@ type InitSection struct {
 
 func (i *InitSection) IsEmpty() bool { return i == nil }
 
+//nolint:unparam
+func (i *InitSection) resolve(p *Profile) {
+	i.FromRepository.setValue(fixPath(i.FromRepository.Value(), expandEnv, expandUserHome))
+}
+
 func (i *InitSection) setRootPath(_ *Profile, rootPath string) {
 	i.FromRepositoryFile = fixPath(i.FromRepositoryFile, expandEnv, expandUserHome, absolutePrefix(rootPath))
 	i.FromPasswordFile = fixPath(i.FromPasswordFile, expandEnv, expandUserHome, absolutePrefix(rootPath))
