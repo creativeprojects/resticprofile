c3a4a74043dc840e63bb448ea8acb160ec1fec78
Fred <Fred@CreativeProjects.Tech>
2024-11-01 21:42:18 +0000

    fix job tests on Windows

--- schedule/job_test.go
+++ schedule/job_test.go
@@ -22,6 +22,7 @@ func TestCreateJobHappyPath(t *testing.T) {
 		ProfileName: "profile",
 		CommandName: "backup",
 		Schedules:   []string{},
+		Permission:  "user",
 	})
 
 	err := job.Create()
@@ -70,6 +71,7 @@ func TestCreateJobErrorCreate(t *testing.T) {
 		ProfileName: "profile",
 		CommandName: "backup",
 		Schedules:   []string{},
+		Permission:  "user",
 	})
 
 	err := job.Create()
