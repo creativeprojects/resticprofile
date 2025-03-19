904115048a8ad266c4af78b40e56aca4912df24e
Fred <fred@creativeprojects.tech>
2025-03-19 15:16:22 +0000

    use root user on system jobs

--- schedule/handler_crond.go
+++ schedule/handler_crond.go
@@ -82,7 +82,11 @@ func (h *HandlerCrond) CreateJob(job *Config, schedules []*calendar.Event, permi
 			job.WorkingDirectory,
 		)
 		if h.config.Username != "" {
-			entries[i] = entries[i].WithUser(h.config.Username)
+			if permission == PermissionSystem {
+				entries[i] = entries[i].WithUser("root")
+			} else {
+				entries[i] = entries[i].WithUser(h.config.Username)
+			}
 		}
 	}
 	crontab := crond.NewCrontab(entries).
