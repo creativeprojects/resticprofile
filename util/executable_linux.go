adf90cbd71229b207d98b55f7669abdea49bc3a0
Fred <fred@creativeprojects.tech>
2025-07-28 18:29:44 +0100

    enhance Executable function to handle relative paths (#537)
    * refactor: enhance Executable function to handle relative paths and symlinks
    
    * refactor: update executable resolution logic and add tests for edge cases

--- util/executable_linux.go
+++ util/executable_linux.go
@@ -5,6 +5,7 @@ package util
 import (
 	"errors"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
@@ -12,17 +13,30 @@ import (
 // On non-Linux systems, it behaves like os.Executable.
 // On Linux, it returns the path to the executable as specified in the command line arguments.
 func Executable() (string, error) {
-	executable := os.Args[0]
+	return resolveExecutable(os.Args[0])
+}
+
+func resolveExecutable(executable string) (string, error) {
 	if len(executable) == 0 {
 		return "", errors.New("executable path is empty")
 	}
 	if executable[0] != '/' {
-		wd, err := os.Getwd()
-		if err != nil {
-			return "", err
+		// If the path is relative, we need to resolve it to an absolute path
+		if executable[0] == '.' {
+			wd, err := os.Getwd()
+			if err != nil {
+				return "", err
+			}
+			// If the executable path is relative, prepend the current working directory to form an absolute path.
+			executable = filepath.Join(wd, executable)
+		} else {
+			// If the path is not absolute, we assume it is in the PATH and resolve it.
+			found, err := exec.LookPath(executable)
+			if err != nil {
+				return "", err
+			}
+			executable = filepath.Clean(found)
 		}
-		// If the executable path is relative, prepend the current working directory to form an absolute path.
-		executable = filepath.Join(wd, executable)
 	}
 	return executable, nil
 }
