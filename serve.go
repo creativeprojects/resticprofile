5f5d522f202511afdda2072db2947816463cd413
Fred <fred@creativeprojects.tech>
2025-08-05 17:42:40 +0100

    refactor: restructure sendProfileCommand and extract sendRemoteFiles for better code organization

--- serve.go
+++ serve.go
@@ -12,8 +12,10 @@ import (
 	"time"
 
 	"github.com/creativeprojects/clog"
+	"github.com/creativeprojects/resticprofile/config"
 	"github.com/creativeprojects/resticprofile/constants"
 	"github.com/creativeprojects/resticprofile/remote"
+	"github.com/creativeprojects/resticprofile/ssh"
 )
 
 func serveCommand(w io.Writer, cmdCtx commandContext) error {
@@ -33,26 +35,7 @@ func serveCommand(w io.Writer, cmdCtx commandContext) error {
 			return
 		}
 
-		// prepare manifest file
-		manifest := remote.Manifest{
-			ConfigurationFile: path.Base(remoteConfig.ConfigurationFile), // need to take file path into consideration
-			ProfileName:       remoteConfig.ProfileName,
-		}
-		manifestData, err := json.Marshal(manifest)
-		if err != nil {
-			sendError(resp, http.StatusInternalServerError, fmt.Errorf("error while generating manifest: %w", err))
-			return
-		}
-
-		clog.Debugf("sending configuration for %q", remoteName)
-		resp.Header().Set("Content-Type", "application/x-tar")
-		resp.WriteHeader(http.StatusOK)
-
-		tar := remote.NewTar(resp)
-		defer tar.Close()
-		_ = tar.SendFiles(append(remoteConfig.SendFiles, remoteConfig.ConfigurationFile))
-		_ = tar.SendFile(constants.ManifestFilename, manifestData)
-
+		sendRemoteFiles(remoteConfig, remoteName, nil, resp)
 	})
 
 	quit := make(chan os.Signal, 1)
@@ -87,6 +70,75 @@ func serveCommand(w io.Writer, cmdCtx commandContext) error {
 		return err
 	}
 	return nil
+}
+
+func sendProfileCommand(w io.Writer, cmdCtx commandContext) error {
+	if len(cmdCtx.flags.resticArgs) < 2 {
+		return fmt.Errorf("missing argument: remote name")
+	}
+	remoteName := cmdCtx.flags.resticArgs[1]
+	if !cmdCtx.config.HasRemote(remoteName) {
+		return fmt.Errorf("remote not found")
+	}
+	remoteConfig, err := cmdCtx.config.GetRemote(remoteName)
+	if err != nil {
+		return err
+	}
+	// send the files to the remote using tar
+	handler := http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		sendRemoteFiles(remoteConfig, remoteName, cmdCtx.flags.resticArgs[2:], resp)
+	})
+	cnx := ssh.NewSSH(ssh.Config{
+		Host:           remoteConfig.Host,
+		Username:       remoteConfig.Username,
+		PrivateKeyPath: remoteConfig.PrivateKeyPath,
+		KnownHostsPath: remoteConfig.KnownHostsPath,
+		Handler:        handler,
+	})
+	defer cnx.Close()
+
+	err = cnx.Connect()
+	if err != nil {
+		return err
+	}
+	binaryPath := remoteConfig.BinaryPath
+	if binaryPath == "" {
+		binaryPath = "resticprofile"
+	}
+	commandLine := fmt.Sprintf("%s -v -r http://localhost:%d/configuration/%s ",
+		binaryPath,
+		cnx.TunnelPort(),
+		remoteName,
+	)
+	err = cnx.Run(commandLine)
+	if err != nil {
+		return fmt.Errorf("failed to run command %q: %w", commandLine, err)
+	}
+	return nil
+}
+
+func sendRemoteFiles(remoteConfig *config.Remote, remoteName string, extraArgs []string, resp http.ResponseWriter) {
+	// prepare manifest file
+	manifest := remote.Manifest{
+		Version:              version,
+		ConfigurationFile:    path.Base(remoteConfig.ConfigurationFile), // need to take file path into consideration
+		ProfileName:          remoteConfig.ProfileName,
+		CommandLineArguments: extraArgs,
+	}
+	manifestData, err := json.Marshal(manifest)
+	if err != nil {
+		sendError(resp, http.StatusInternalServerError, fmt.Errorf("error while generating manifest: %w", err))
+		return
+	}
+
+	clog.Debugf("sending configuration for %q", remoteName)
+	resp.Header().Set("Content-Type", "application/x-tar")
+	resp.WriteHeader(http.StatusOK)
+
+	tar := remote.NewTar(resp)
+	defer tar.Close()
+	_ = tar.SendFiles(append(remoteConfig.SendFiles, remoteConfig.ConfigurationFile))
+	_ = tar.SendFile(constants.ManifestFilename, manifestData)
 }
 
 func sendError(resp http.ResponseWriter, status int, err error) {
