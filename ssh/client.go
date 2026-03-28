3ee3b6b4249116178b2648834ed155daa553fe72
Fred <fred@creativeprojects.tech>
2026-03-28 22:42:49 +0000

    refactor: standardize parameter naming in Client interface methods

--- ssh/client.go
+++ ssh/client.go
@@ -4,8 +4,8 @@ import "context"
 
 type Client interface {
 	Name() string
-	Connect(context.Context) error
-	Close(context.Context)
+	Connect(ctx context.Context) error
+	Close(ctx context.Context)
 	Run(command string, arguments ...string) error
 	TunnelPeerPort() int
 }
