e663136f396d461823700c9a0eb8b6c30885676f
Fred <fred@creativeprojects.tech>
2026-04-17 18:47:57 +0100

    fix: update build constraints for executable files to correctly handle BSD and Linux

--- util/executable.go
+++ util/executable.go
@@ -1,12 +1,12 @@
-//go:build !linux
+//go:build darwin || windows
 
 package util
 
 import "os"
 
 // Executable returns the path name for the executable that started the current process.
-// On non-Linux systems, it behaves like os.Executable.
-// On Linux, it returns the path to the executable as specified in the command line arguments.
+// On Darwin and Windows, it behaves like os.Executable.
+// On Linux and BSDs, it returns the path to the executable as specified in the command line arguments.
 func Executable() (string, error) {
 	return os.Executable()
 }
