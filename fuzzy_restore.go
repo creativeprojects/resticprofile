157f40e4b925b7d526bb76f4ab2514b231095c68
Fred <fred@creativeprojects.tech>
2026-06-30 22:21:01 +0100

    display list of files from repo

--- fuzzy_restore.go
+++ fuzzy_restore.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
 	"sync"
 	"time"
 
+	"github.com/creativeprojects/clog"
 	"github.com/ktr0731/go-fuzzyfinder"
+)
+
+const (
+	preview = `
+path: %s
+file: %s
+mode: %s
+user: %s
+grp:  %s
+size: %d
+time: %s
+`
 )
 
 type RestorableFile struct {
@@ -19,16 +39,67 @@ type RestorableFile struct {
 
 func fuzzyRestore(cmdCtx commandContext) error {
 	lock := new(sync.Mutex)
-	files := make([]RestorableFile, 0, 100)
-	go func() {
-		for i := range 100 {
+	files := make([]RestorableFile, 0)
+
+	command := "ls"
+	profile, cleanup, err := openProfile(cmdCtx.config, cmdCtx.request.profile)
+	defer cleanup()
+	if err != nil {
+		return err
+	}
+	cmdCtx.profile = profile
+
+	cmdCtx.request.arguments = append(cmdCtx.request.arguments, "latest", "--long")
+
+	wrapper := newResticWrapper(&cmdCtx.Context)
+	args := profile.GetCommandFlags(command)
+	rCommand := wrapper.prepareCommand(command, args, true)
+	cmd := exec.CommandContext(context.TODO(), rCommand.command, rCommand.args...)
+	reader, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+	defer cmd.Wait()
+
+	scanner := bufio.NewScanner(reader)
+
+	gotOne := make(chan struct{})
+	finished := make(chan struct{})
+	firstOne := sync.OnceFunc(func() {
+		close(gotOne)
+	})
+	wg := new(sync.WaitGroup)
+	wg.Go(func() {
+		for scanner.Scan() {
+			file := RestorableFile{}
+			var datePart, timePart string
+			_, err := fmt.Sscanf(scanner.Text(), "%s %s %s %d %s %s %s", &file.mode, &file.uid, &file.gid, &file.size, &datePart, &timePart, &file.path)
+			if err != nil {
+				continue
+			}
+			file.date, _ = time.Parse(time.DateTime, datePart+" "+timePart)
 			lock.Lock()
-			files = append(files, RestorableFile{path: fmt.Sprintf("file %d", i)})
+			files = append(files, file)
 			lock.Unlock()
-			time.Sleep(10 * time.Millisecond)
+			firstOne()
 		}
-	}()
-	time.Sleep(20 * time.Millisecond)
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading file list:", err)
+		}
+		close(finished)
+	})
+	defer wg.Wait()
+
+	// wait for at least one file to come in before displaying fuzzy finder
+	select {
+	case <-gotOne:
+	case <-finished:
+		return errors.New("no file to restore")
+	}
+
 	selected, err := fuzzyfinder.FindMulti(&files, func(i int) string {
 		if i < 0 {
 			return "error"
@@ -40,11 +111,32 @@ func fuzzyRestore(cmdCtx commandContext) error {
 			if i < 0 {
 				return "N/A"
 			}
-			return fmt.Sprintf("mode: %s\npath: %s", files[i].mode, files[i].path)
+			uid, gid := files[i].uid, files[i].gid
+			usr, err := user.LookupId(uid)
+			if err == nil {
+				uid = usr.Username
+			}
+			grp, err := user.LookupGroupId(gid)
+			if err == nil {
+				gid = grp.Name
+			}
+			return fmt.Sprintf(preview,
+				filepath.Dir(files[i].path),
+				filepath.Base(files[i].path),
+				files[i].mode,
+				uid,
+				gid,
+				files[i].size,
+				files[i].date.Format(time.DateTime),
+			)
 		}),
 		fuzzyfinder.WithHotReloadLock(lock),
 	)
 	if err != nil {
+		if errors.Is(err, fuzzyfinder.ErrAbort) {
+			clog.Info("operation cancelled")
+			return nil
+		}
 		return err
 	}
 	fmt.Printf("%+v", selected)
